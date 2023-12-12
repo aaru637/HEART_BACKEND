01e20ebc82b5ea8874a2edb4795d89dcc0ba1eaf
DK <dhandapanisakthi123@gmail.com>
2023-12-12 19:49:25 +0530

    Source Link Change

--- src/main/java/com/heart/heart/Concrete/Urls.java
+++ src/main/java/com/heart/heart/Concrete/Urls.java
@@ -1,7 +1,7 @@
 package com.heart.heart.Concrete;
 
 public class Urls {
-    public final static String BASE_URL = "http://localhost:8080/";
+    public final static String BASE_URL = "https://heart-bhp5.onrender.com/";
     public final static String ADMIN_BASE_URL = BASE_URL + "admin/";
     public final static String STUDENT_BASE_URL = BASE_URL + "student/";
 }
